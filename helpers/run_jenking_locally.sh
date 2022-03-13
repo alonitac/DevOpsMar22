7377899554ca478b949a0e6f93a6ec1e85f46c8f
Alon Itach <alon.itach@mail.huji.ac.il>
2022-03-13 08:37:02 +0200

    test & release stages

--- helpers/run_jenking_locally.sh
+++ helpers/run_jenking_locally.sh
@@ -1,12 +1,12 @@
-docker run --name jenkins-blueocean -u root --rm -d -p 8080:8080 -v jenkins-data:/var/jenkins_home \
--v /var/run/docker.sock:/var/run/docker.sock jenkinsci/blueocean
+docker run --name jenkins -u root --rm -d -p 8080:8080 -v jenkins-data:/var/jenkins_home \
+-v /var/run/docker.sock:/var/run/docker.sock jenkins/jenkins
 
 # NOTES
 
 # --name  Gives a name to the running container
 # --rm  Automatically removes the Docker container when it is shut down.
-# -d    Runs the jenkinsci/blueocean container in the background ("detached" mode)
-# -p    Makes (i.e. "publishes") port 8080 of the jenkinsci/blueocean container to port 8080 on the
+# -d    Runs the jenkins/jenkins container in the background ("detached" mode)
+# -p    Makes (i.e. "publishes") port 8080 of the jenkins/jenkins container to port 8080 on the
 #       host machine. The 1st number represents the port on the host while the 2nd represents the
 #       container’s port.
 # -v    Maps the /var/jenkins_home directory in the container to the Docker volume with the name
