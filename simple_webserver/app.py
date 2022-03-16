6a34810cefbb0480b7f6a0d2f51c3fabda1237e9
Alon Itach <alon.itach@mail.huji.ac.il>
2022-03-16 13:51:46 +0200

    k8s manifests fixes

--- simple_webserver/app.py
+++ simple_webserver/app.py
@@ -1,5 +1,9 @@
 from flask import Flask
 import os
+import signal
+import time
+import sys
+
 
 app = Flask(__name__)
 
@@ -14,6 +18,22 @@ def hello(name):
     return f"Hello, {name}!"
 
 
+def signal_handler(sig, frame):
+    print(f'Got signal number {sig} {frame}')
+    print('Handling the last server requests...')
+    time.sleep(4)
+    print('Serve is closed for new requests')
+    time.sleep(1)
+
+    print('Disconnecting from database..')
+    time.sleep(3)
+    print('Successfully disconnected from db')
+    print('Performing other cleanup tasks...')
+    time.sleep(7)
+    sys.exit(0)
+
+
 if __name__ == '__main__':
+    signal.signal(signal.SIGINT, signal_handler)
     app.run(host='0.0.0.0', port=8080)
 
